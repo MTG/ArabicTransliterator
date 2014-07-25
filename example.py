e64c6cad7d3e4de054fd479264ebc9ee2ab7b3eb
Mohamed Sordo <mohamed.sordo@gmail.com>
2014-07-25 11:44:09 +0200

    Added licence text

--- example.py
+++ example.py
@@ -1,5 +1,24 @@
 # -*- coding: utf-8 -*-
 
+# This file is part of ArabicTransliterator.
+#
+# ArabicTransliterator is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ArabicTransliterator is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ArabicTransliterator.  If not, see <http://www.gnu.org/licenses/>.
+
+# Written by Mohamed Sordo (@neomoha)
+# Email: mohamed ^dot^ sordo ^at^ gmail ^dot^ com
+# Website: http://msordo.weebly.com
+
 import codecs, platform
 import arabic_reshaper #
 from bidi.algorithm import get_display
@@ -9,15 +28,15 @@ sys.path.append('mishkal/')
 sys.path.append('mishkal/lib/')
 import tashkeel.tashkeel as tashkeel
 
-def print_reshape(text):
+def reshape(text):
     """Reshapes arabic in order to display characters from right to left
     """
     if platform.system() == "Darwin":
-        print text
+        return text
     else:
         reshaped_text = arabic_reshaper.reshape(text)
         bidi_text = get_display(reshaped_text)
-        print bidi_text
+        return bidi_text
 
 if __name__ == '__main__':
     f = codecs.open("example_arabic_script.dat", "r", "utf-8")
@@ -25,14 +44,14 @@ if __name__ == '__main__':
     for line in f:
         print "--------------Original Text--------------"
         text = line.strip()
-        print_reshape(text)
+        print reshape(text)
         print "--------------Vocalized Text--------------"
         vocalizer=tashkeel.TashkeelClass()
         voc = vocalizer.tashkeel(text)
-        print_reshape(voc)
+        print reshape(voc)
         print "--------------Transliterated Text--------------"
         tr = transliterator.do(voc.strip())
-        print_reshape(tr)
+        print reshape(tr)
         print "#########################################"
     f.close()
 
