b386b197d04b8285d08d32c4a4f2dc9de0d015de
Andres Ferraro <andres.ferraro@upf.edu>
2015-07-14 17:48:37 +0200

    Adding setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='arabictransliterator',
+      version='0.1',
+      description='A code for transliterating (romanizing) Arabic text using the ALA-LC standard',
+      author='CompMusic / MTG UPF',
+      url='http://compmusic.upf.edu',
+      packages=find_packages(include=["mishkal"])
+)
