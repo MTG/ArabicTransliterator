a847c8acb1d021ed07e919a514fa1a89b523684e
Andres Ferraro <andres.ferraro@upf.edu>
2015-08-27 11:09:19 +0200

    Changes on setup.py

--- setup.py
+++ setup.py
@@ -7,14 +7,6 @@ setup(name='arabictransliterator',
       description='A code for transliterating (romanizing) Arabic text using the ALA-LC standard',
       author='CompMusic / MTG UPF',
       url='http://compmusic.upf.edu',
-      packages=["tashkeel", "arabic", 'libqutrub', 'pyarabic', 'naftawayh', 'tashaphyne', 'qalsadi' ],
-      package_dir={
-              'tashaphyne':'mishkal/lib/tashaphyne',
-              'naftawayh':'mishkal/lib/naftawayh', 
-              'pyarabic':'mishkal/lib/pyarabic',
-              'libqutrub':'mishkal/lib/qalsadi/libqutrub',
-              'qalsadi':'mishkal/lib/qalsadi', 
-              'arabic':'', 
-              'tashkeel': 'mishkal/tashkeel' },
+      packages=find_packages(),
 
 )
