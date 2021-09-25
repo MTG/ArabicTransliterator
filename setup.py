a0140eb8c617a37600d3eadd9a11a6a3c35a8403
Gokul NC <gokulnc@ymail.com>
2021-09-25 13:17:51 +0530

    Package the transliterator

--- setup.py
+++ setup.py
@@ -1,12 +1,34 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
+import pkg_resources
 
-setup(name='arabictransliterator',
-      version='0.3',
+with open("requirements.txt") as requirements_txt:
+    install_requires = [
+        str(requirement)
+        for requirement
+        in pkg_resources.parse_requirements(requirements_txt)
+    ]
+
+# Read meta-data
+about = {}
+exec(open('arabic_transliterator/__version.py').read(), about)
+
+setup(name='arabic-transliterator',
+      version=about["__version__"],
       description='A code for transliterating (romanizing) Arabic text using the ALA-LC standard',
+      long_description=open("README.rst", encoding="utf-8").read(),
+      long_description_content_type="text/x-rst",
+
       author='CompMusic / MTG UPF',
       url='http://compmusic.upf.edu',
+
+      packages=["arabic_transliterator"],
       include_package_data=True,
-      
+      install_requires=install_requires,
+      classifiers=[
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries"
+      ],
 )
